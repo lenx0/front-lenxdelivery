18fa1b7bd95c95fa86e513bcace68eae81cb4110
Thiago <thiagofreitas1106@gmail.com>
2023-07-28 18:52:20 -0300

    first commit

---
+++ src/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import logo from "./logo.png";
+
+export default function Navbar() {
+  return (
+    <nav className="main-navbar">
+      <img src={logo} alt="Logo do site" />
+      <a href="home" className="logo-text">Lenx Delivery</a>
+    </nav>
+  );
+}
