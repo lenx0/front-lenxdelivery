d0cc12a0a4096e74fcb96e53166571ea5693d9fd
Thiago <thiagofreitas1106@gmail.com>
2023-08-09 23:02:48 -0300

    save

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:3100'
+// const API_URL = 'http://localhost:3100'
+const API_URL = 'http://18.231.66.105:3100'
 
 export function fetchProducts() {
     return axios(`${API_URL}/v1/lenxdelivery/dish/list`)
