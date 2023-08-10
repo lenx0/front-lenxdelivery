488fef525a3b1d3ea76b3da76f15f2ec66846b04
Thiago <thiagofreitas1106@gmail.com>
2023-08-09 23:44:18 -0300

    http test

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = 'http://localhost:3100'
-const API_URL = 'https://ec2-18-230-65-121.sa-east-1.compute.amazonaws.com:3100'
+const API_URL = 'http://ec2-18-230-65-121.sa-east-1.compute.amazonaws.com:3100'
 
 export function fetchProducts() {
     return axios(`${API_URL}/v1/lenxdelivery/dish/list`)
