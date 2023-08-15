e32a5c9f8d702c0a71b2f59668af80a1abb0637f
Thiago <thiagofreitas1106@gmail.com>
2023-08-15 15:48:10 -0300

    fix api url

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:3100'
+// const API_URL = 'http://localhost:3100'
 // const API_URL = 'http://ec2-18-230-65-121.sa-east-1.compute.amazonaws.com:3100'
+const API_URL = 'https://lenxdelivery-api.onrender.com:3100'
+
+
 
 export function fetchProducts() {
     return axios(`${API_URL}/v1/lenxdelivery/dish/list`)
